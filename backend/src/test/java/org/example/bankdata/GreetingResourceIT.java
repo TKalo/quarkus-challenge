2c72e7916be5b27d6c33a1251d3f65f33a62044f
Tkalo <troelskaldau@live.dk>
2025-01-26 12:55:35 +0100

    first commit

---
+++ backend/src/test/java/org/example/bankdata/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.example.bankdata;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
