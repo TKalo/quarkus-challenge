b9772de10046caa9192803c1dbaf20b03e4493fc
Tkalo <troelskaldau@live.dk>
2025-01-26 15:36:15 +0100

    tested deposite endpoint

--- backend/src/test/java/org/example/bankdata/AccountResourceTest.java
+++ backend/src/test/java/org/example/bankdata/AccountResourceTest.java
@@ -1,6 +1,8 @@
 package org.example.bankdata;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
 import jakarta.inject.Inject;
 
 @QuarkusTest
@@ -45,5 +48,73 @@ class AccountResourceTest {
         assertEquals(responseAccount.getLastName(), databaseAccount.getLastName());
         assertEquals(responseAccount.getAccountNumber(), databaseAccount.getAccountNumber());
         assertEquals(responseAccount.getBalance(), databaseAccount.getBalance());
+    }
+
+    @Test
+    public void testDepositMoneySuccess() {
+        AccountInput input = new AccountInput();
+        input.setFirstName("John");
+        input.setLastName("Doe");
+
+        Account account = RestAssured.given()
+                .contentType("application/json")
+                .body(input)
+                .when()
+                .post("/account")
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(Account.class);
+
+        double depositAmount = 100.0;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(depositAmount)
+                .when()
+                .post("/account/" + account.getAccountNumber() + "/deposit")
+                .then()
+                .statusCode(200)
+                .body("balance", equalTo((float) depositAmount));
+    }
+
+    @Test
+    public void testDepositNegativeAmount() {
+        AccountInput input = new AccountInput();
+        input.setFirstName("Jane");
+        input.setLastName("Smith");
+
+        Account account = RestAssured.given()
+                .contentType("application/json")
+                .body(input)
+                .when()
+                .post("/account")
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(Account.class);
+
+        double negativeAmount = 0;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(negativeAmount)
+                .when()
+                .post("/account/" + account.getAccountNumber() + "/deposit")
+                .then()
+                .statusCode(400)
+                .body("error", containsString(("Amount must be greater than 0")));
+    }
+
+    @Test
+    public void testDepositToNonExistingAccount() {
+        String invalidAccountNumber = "non-existing-account";
+        double depositAmount = 100.0;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(depositAmount)
+                .when()
+                .post("/account/" + invalidAccountNumber + "/deposit")
+                .then()
+                .statusCode(404);
     }
 }
